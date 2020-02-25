027bdfb35258d9bb4c323d782d2de19f09efc856
Sean <sdoran35@gmail.com>
2020-02-24 23:17:52 -0600

    Kata Completed thru Step 6. Will TDD Steps 6-9 on 2/25/20

---
+++ StringCalculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace StringCalculator
+{
+    class StringCalculator
+    {
+      
+
+        public static int Add(string numbers)
+        {
+            var delims = ",\n";
+            if(string.IsNullOrEmpty(numbers))
+                return 0;
+            if (numbers.Contains("//"))
+            {
+                delims += numbers[2];
+                numbers = numbers.Substring(4, numbers.Length - 4);
+
+            }
+            var items = numbers.Split(delims.ToCharArray());
+            if(items.Any(string.IsNullOrEmpty))
+                throw new ArgumentException();
+
+            var integers = items.Select(x => int.Parse(x));
+            var negatives = integers.Where(x => x < 0);
+            if (!negatives.Any()) return items.Select(x => int.Parse(x)).Sum();
+            {
+                var message = "Negative not allowed: {0}";
+                throw new ArgumentOutOfRangeException(string.Format(message,string.Join(",", negatives.Select(x => x.ToString()).ToArray())));
+            }
+            
+           
+
+
+        }
+    }
+}
---
+++ StringCalculatorTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Xunit;
+
+namespace StringCalculator
+{
+    public class StringCalculatorTest
+    {
+        
+        [Fact]
+        public void GivenEmptyStringReturnZero()
+        {
+            string.Empty.ShouldCalculateTo(0);
+
+        }
+
+        [Fact]
+        public void GivenSingleNumberReturnNumber()
+        {
+            "1".ShouldCalculateTo(1);
+        }
+
+        [Fact]
+        public void GivenTwoNumbersReturnSumOfBothNumbers()
+        {
+            "1,2".ShouldCalculateTo(3);
+        }
+
+        [Fact]
+        public void GivenMultipleNumbersReturnSum()
+        {
+            "1,2,3".ShouldCalculateTo(6);
+        }
+
+        [Fact]
+        public void GivenNewLineDelimReturnSum()
+        {
+            "1\n2\n3".ShouldCalculateTo(6);
+        }
+
+        [Fact]
+        public void GivenConsecutiveDelimsThrowException()
+        {
+            Assert.Throws<ArgumentException>(() => StringCalculator.Add(",,"));
+        }
+
+        [Fact]
+        public void GivenCustomDelimReturnSum()
+        {
+            "//;\n1;2".ShouldCalculateTo(3);
+        }
+
+        [Fact]
+        public void GivenNegNumberThrowException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => StringCalculator.Add("-1,2"));
+        }
+
+        [Fact]
+        public void GivenNegNumberErrorMessageShouldContainNegNumber()
+        {
+            var error = Assert.Throws<ArgumentOutOfRangeException>(() => StringCalculator.Add("-1,2"));
+            Assert.Contains("-1", error.Message);
+        }
+
+        [Fact]
+        public void IgnoreNumbersGreaterThan1000()
+        {
+            "1001,2".ShouldCalculateTo(2);
+            
+        }
+
+
+    }
+
+    internal static class TestHelper
+    {
+        public static void ShouldCalculateTo(this string input, int expected)
+        {
+            var item = new StringCalculator();
+            Assert.Equal(expected,StringCalculator.Add(input));
+            
+        }
+    }
+}
